7e90b4626ac580616b24d7da8cd2f2157b051eb4
baloghgergo96 <baloghgergo0825@gmail.com>
2021-11-10 14:25:34 +0100

    Test

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,15 @@
+const removeDuplicatesFromArray = (list) => {
+    let noDuplicationArray = [];
+    for (let i=0; i < list.length; i++){
+        if (list[i] == list[i+1]){
+            delete list[i]
+        }
+        else{
+            noDuplicationArray.push(list[i])
+        }
+    }
+    return noDuplicationArray;
+}
 
 
-
-
-
-
-// export default removeDuplicatesFromArray
+export default removeDuplicatesFromArray
--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,7 @@
+const HU = {
+    date(date) {date => new Intl.DateTimeFormat('hu-HU').format(date)}, 
+    currency(number) {number => new Intl.NumberFormat('hu-HU', {style: 'currency', currency : 'Ft'})},
+    list(array) {array => [array[0], array[1], array[2]]},
+};
 
-
-
-
-
-
-// export default HU
+export default HU
